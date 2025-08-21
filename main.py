16c4a3b3734012276679998b61c82e47bb045170
CookieCrumble <rndud329@gmail.com>
2025-08-21 23:15:42 +0900

    test done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 def main():
+    load_dotenv()
     print("Hello from ai-chatbot!")
+    SECRETE_ENV = os.getenv("elevenlabs_api")
+    print(SECRETE_ENV)
 
 
 if __name__ == "__main__":
