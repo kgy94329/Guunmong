464748551113c8cec657b0f6cb88635f6a267459
Cookiecrumble <rndud329@gmail.com>
2025-09-14 15:46:42 +0900

    add tts api

--- functions/tts_functions.py
+++ functions/tts_functions.py
@@ -2,27 +2,116 @@ import base64
 import datetime
 import os
 import requests
-from elevenlabs import ElevenLabs
-from elevenlabs import save
-from dotenv import load_dotenv
 import time
+import wave
+import io
+from pydub import AudioSegment
 
-load_dotenv()
+from abc import ABC, abstractmethod
 
-TTS_API_KEY = os.getenv('elevenlabs_api')
+from elevenlabs import ElevenLabs
+from dotenv import load_dotenv
 
-elevenlabs = ElevenLabs(
-    api_key = TTS_API_KEY
-)
+from google import genai
+from google.genai import types
 
-def create_voice(text):
-    print(f'[음성 합성 작업시작] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    audio_low_data = elevenlabs.text_to_speech.convert(
-        voice_id="uyVNoMrnUku1dZyVEXwD",
-        output_format="mp3_44100_128",
-        text= text,
-        model_id="eleven_multilingual_v2",
-    )
-    audio = base64.b64encode(b''.join(audio_low_data)).decode()
-    print(f'[음성 합성 작업종료] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    return audio
+ELEVENLABS_VOCAL = {'Anna Kim': 'uyVNoMrnUku1dZyVEXwD'}
+
+class TTSStrategy(ABC):
+    @abstractmethod
+    def create_voice(self, text, vocal):
+        pass
+
+class ElevenLabsTTS(TTSStrategy):
+    def __init__(self, api_key, vocal):
+        self.api_key = api_key
+        self.vocal = ELEVENLABS_VOCAL[vocal]
+        self.elevenlabs = ElevenLabs(
+            api_key = api_key
+        )
+
+    def create_voice(self, text):
+        print(f'[음성 합성 작업시작] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+        # TTS 처리 요청
+        audio_low_data = self.elevenlabs.text_to_speech.convert(
+            voice_id=self.vocal,
+            output_format="mp3_44100_128",
+            text= text,
+            model_id="eleven_multilingual_v2",
+        )
+
+        # 음성 데이터 처리
+        audio = base64.b64encode(b''.join(audio_low_data)).decode()
+        print(f'[음성 합성 작업종료] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        return audio
+
+class GoogleTTS(TTSStrategy):
+    def __init__(self, api_key, vocal):
+        self.api_key = api_key
+        self.vocal = vocal
+        self.client = genai.Client(api_key=api_key)
+
+    def create_voice(self, text):
+        print(f'[음성 합성 작업시작] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+
+        # TTS 처리 요청
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash-preview-tts",
+            contents=text,
+            config=types.GenerateContentConfig(
+                response_modalities=["AUDIO"],
+                speech_config=types.SpeechConfig(
+                    voice_config=types.VoiceConfig(
+                        prebuilt_voice_config=types.PrebuiltVoiceConfig(
+                        voice_name=self.vocal,
+                        )
+                    )
+                ),
+            )
+        )
+
+        # 음성데이터 처리
+        audio = response.candidates[0].content.parts[0].inline_data.data
+        audio_segment = AudioSegment(
+            data=audio,
+            sample_width=2,
+            frame_rate=24000,
+            channels=1
+        )
+        buffer = io.BytesIO()
+        audio_segment.export(buffer, format="mp3")  # or "wav"
+        b64_audio = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        print(f'[음성 합성 작업종료] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        
+        return b64_audio
+
+class TTSSynthesizer:
+    def __init__(self, strategy: TTSStrategy, vocal):
+        self.strategy = strategy
+        self.vocal = vocal
+
+    def set_strategy(self, strategy: TTSStrategy):
+        self.strategy = strategy
+
+    def create_voice(self, text):
+        return self.strategy.create_voice(text)
+    
+def get_synthesizer(model_name, vocal):
+    load_dotenv()
+
+    # user_choice = "google"  # 예: 사용자가 선택한 옵션
+    if model_name == "Google":
+        API_KEY = os.getenv('google_ai_studio_api')
+        strategy = GoogleTTS(api_key=API_KEY, vocal=vocal)
+    elif model_name == "ElevenLabs":
+        API_KEY = os.getenv('elevenlabs_api')
+        strategy = ElevenLabsTTS(api_key=API_KEY, vocal=vocal)
+    else:
+        raise ValueError("지원되지 않는 TTS 엔진")
+
+    tts = TTSSynthesizer(strategy, vocal)
+    return tts
+
+if __name__ == "__main__":
+    main()
--- pages/chatting_page.py
+++ pages/chatting_page.py
@@ -2,18 +2,36 @@ import streamlit as st
 import time
 import datetime
 import base64
-from functions.tts_functions import create_voice
+from functions.tts_functions import get_synthesizer
 from functions.llm_functions import create_response
 import tempfile
 
+# 최초 실행 시만 설정
+if "tts_model" not in st.session_state:
+    st.session_state.tts_model = get_synthesizer('ElevenLabs', 'Anna Kim')
+
 # 오디오 자동 재생 JS 생성 함수
 def auto_play_audio(data):
+    print('음성 출력')
     audio_html = f"""
     <audio autoplay>
         <source src="data:audio/mp3;base64,{data}" type="audio/mp3">
     </audio>
     """
     st.markdown(audio_html, unsafe_allow_html=True)
+
+# 채팅 설정창
+with st.popover(":material/instant_mix: 음성 설정"):
+    model_option = st.selectbox("음성 모델 선택", ('Google', 'ElevenLabs'))
+    vocal_option = ''
+    if model_option == 'Google':
+        vocal_option = st.selectbox("음성 선택", ('Kore'))
+    else:
+        vocal_option = st.selectbox('음성 선택', ('Anna Kim'))
+
+    if st.button('적용', type='primary'):
+        st.session_state.tts_model = get_synthesizer(model_option, vocal_option)
+        st.write(f'{model_option} 모델의 {vocal_option}이 적용되었습니다.')
 
 # 채팅 기록 출력
 for msg in st.session_state["messages"]:
@@ -23,7 +41,7 @@ for msg in st.session_state["messages"]:
     else:
         with st.chat_message('assistant'):
             st.markdown(msg['content'])
-            
+
 # 입력창
 if prompt := st.chat_input('메시지를 입력하세요...'):
     # 사용자 메시지 저장
@@ -33,23 +51,21 @@ if prompt := st.chat_input('메시지를 입력하세요...'):
     with st.chat_message('user'):
         st.markdown(prompt)
     
-    # 간단한 응답
-    # response = prompt
-    
-
     with st.chat_message("assistant"):
         print(f'[작업시작] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
         placeholder = st.empty()
         placeholder.markdown("⏳ 음성 변환 중...")
         
         # GPT 답변 생성
-        response = create_response(prompt)
+        # response = create_response(prompt)
+
+        # 테스트 코드
+        response = prompt
         
         # 음성 합성
-        tts = create_voice(response)
+        tts_model = st.session_state.tts_model
+        voice = tts_model.create_voice(response)
         
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
-            auto_play_audio(tts)
         print(f'[작업종료] {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
             
     # 챗봇 메시지 출력
@@ -59,6 +75,7 @@ if prompt := st.chat_input('메시지를 입력하세요...'):
                 yield word + " "
                 time.sleep(0.02)
         placeholder.write_stream(stream_date)
+        auto_play_audio(voice)
         
     # 챗봇 응답 저장
     st.session_state['messages'].append({'role': 'assistant', 'content': response})
